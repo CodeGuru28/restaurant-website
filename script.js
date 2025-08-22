2ee08ba0a50e1a30eb1c7150a633036580505678
Guru Code <codeguru28@gmail.com>
2025-08-22 23:33:44 +0530

    Responsive and Fully Functional Nav

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ navLinks.forEach(link => {
         link.classList.add('active');
     });
 })
+
+const bar = document.querySelector('.bar');
+const menu = document.querySelector('.menu');
+const body = document.body;
+bar.addEventListener('click', () => {
+  if(menu.classList.contains('active-menu')) {
+    menu.classList.remove('active-menu');
+    body.classList.remove('menu-open'); // Remove class when closing menu
+  } else {
+    menu.classList.add('active-menu');
+    body.classList.add('menu-open'); // Add class when opening menu
+  }
+});
+
+const links = document.querySelectorAll('.link');
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('active-menu');
+    body.classList.remove('menu-open'); // Remove class when clicking a link
+  })
+});
